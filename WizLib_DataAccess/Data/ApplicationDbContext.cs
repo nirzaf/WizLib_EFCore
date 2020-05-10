ee6b8abb1a9d5a1f102f9ac762579e4fc1062cb3
Ben Spark <bhrugen1990@gmail.com>
2020-05-10 16:56:58 -0500

    Section 4 - Primary Key and Required

--- WizLib_DataAccess/Data/ApplicationDbContext.cs
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -21,12 +21,19 @@ namespace WizLib_DataAccess.Data
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }
 
+        public DbSet<Fluent_BookDetail> Fluent_BookDetails { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //we configure fluent API
 
             //composite key
-            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.Author_Id,ba.Book_Id });
+            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.Author_Id, ba.Book_Id });
+
+
+            //BookDetails
+            modelBuilder.Entity<Fluent_BookDetail>().HasKey(b => b.BookDetail_Id);
+            modelBuilder.Entity<Fluent_BookDetail>().Property(b => b.NumberOfChapters).IsRequired();
         }
     }
 }
---
+++ WizLib_DataAccess/Migrations/20200510215532_AddFluentBookDetailsToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class AddFluentBookDetailsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fluent_BookDetails",
+                columns: table => new
+                {
+                    BookDetail_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfChapters = table.Column<int>(type: "int", nullable: false),
+                    NumberOfPages = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fluent_BookDetails", x => x.BookDetail_Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fluent_BookDetails");
+        }
+    }
+}
