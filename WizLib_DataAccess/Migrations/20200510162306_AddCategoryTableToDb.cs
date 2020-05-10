38924e21afe47209c67b3c50ad8b2e4f97ce28f4
Ben Spark <bhrugen1990@gmail.com>
2020-05-10 11:23:49 -0500

    Section 2 - Add Migration

---
+++ WizLib_DataAccess/Migrations/20200510162306_AddCategoryTableToDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class AddCategoryTableToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
