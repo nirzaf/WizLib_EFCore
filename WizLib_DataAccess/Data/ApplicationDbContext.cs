e648581b4654232c856d2f198569f726353dacd9
Ben Spark <bhrugen1990@gmail.com>
2020-05-10 17:02:19 -0500

    Section 4 - Smartness of EF Core

--- WizLib_DataAccess/Data/ApplicationDbContext.cs
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace WizLib_DataAccess.Data
         //public DbSet<Category> Categories { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }
---
+++ WizLib_DataAccess/Migrations/20200510220115_BookDetailsToDb.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class BookDetailsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookDetail_BookDetail_Id",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookDetail",
+                table: "BookDetail");
+
+            migrationBuilder.RenameTable(
+                name: "BookDetail",
+                newName: "BookDetails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookDetails",
+                table: "BookDetails",
+                column: "BookDetail_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookDetails_BookDetail_Id",
+                table: "Books",
+                column: "BookDetail_Id",
+                principalTable: "BookDetails",
+                principalColumn: "BookDetail_Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookDetails_BookDetail_Id",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookDetails",
+                table: "BookDetails");
+
+            migrationBuilder.RenameTable(
+                name: "BookDetails",
+                newName: "BookDetail");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookDetail",
+                table: "BookDetail",
+                column: "BookDetail_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookDetail_BookDetail_Id",
+                table: "Books",
+                column: "BookDetail_Id",
+                principalTable: "BookDetail",
+                principalColumn: "BookDetail_Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
