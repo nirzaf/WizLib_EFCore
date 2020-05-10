50d093eca0b858ef60a4f81a4a2dab5a90e7bb88
Ben Spark <bhrugen1990@gmail.com>
2020-05-10 14:04:30 -0500

    Section 3 - Table Name and Column Name

---
+++ WizLib_DataAccess/Migrations/20200510190255_ChangeTableAndColumnNamesOfGenreTb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class ChangeTableAndColumnNamesOfGenreTb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "tb_Genre");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreName",
+                table: "tb_Genre",
+                newName: "Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tb_Genre",
+                table: "tb_Genre",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tb_Genre",
+                table: "tb_Genre");
+
+            migrationBuilder.RenameTable(
+                name: "tb_Genre",
+                newName: "Genres");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Genres",
+                newName: "GenreName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "GenreId");
+        }
+    }
+}
--- WizLib_Model/Models/Genre.cs
+++ WizLib_Model/Models/Genre.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WizLib_Model.Models
 {
+    [Table("tb_Genre")]
     public class Genre
     {
         public int GenreId { get; set; }
+        [Column("Name")]
         public string GenreName { get; set; }
         //public int DisplayOrder { get; set; }
     }
