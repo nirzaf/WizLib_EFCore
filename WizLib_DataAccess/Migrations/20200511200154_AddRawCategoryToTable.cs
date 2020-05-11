e90783a936be220717bc760b87e4c0619b8631b3
Ben Spark <bhrugen1990@gmail.com>
2020-05-11 15:07:14 -0500

    Section 5 - Insert Record with Migrations and Logging

---
+++ WizLib_DataAccess/Migrations/20200511200154_AddRawCategoryToTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class AddRawCategoryToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 1')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 2')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
