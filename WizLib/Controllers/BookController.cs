158a9d9ae4b0b5a7d1fa3e29b40cf10933e93edc
Ben Spark <bhrugen1990@gmail.com>
2020-05-11 18:06:06 -0500

    Section 5 - Book Index Action

---
+++ WizLib/Controllers/BookController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WizLib_DataAccess.Data;
+using WizLib_DataAccess.Migrations;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> objList = _db.Books.ToList();
+            return View(objList);
+        }
+
+        //public IActionResult Upsert(int? id)
+        //{
+        //    Author obj = new Author();
+        //    if (id == null)
+        //    {
+        //        return View(obj);
+        //    }
+        //    //this for edit
+        //    obj = _db.Authors.FirstOrDefault(u => u.Author_Id == id);
+        //    if (obj == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(obj);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Author obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (obj.Author_Id == 0)
+        //        {
+        //            //this is create
+        //            _db.Authors.Add(obj);
+        //        }
+        //        else
+        //        {
+        //            //this is an update
+        //            _db.Authors.Update(obj);
+        //        }
+        //        _db.SaveChanges();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(obj);
+
+        //}
+
+        //public IActionResult Delete(int id)
+        //{
+        //    var objFromDb = _db.Authors.FirstOrDefault(u => u.Author_Id == id);
+        //    _db.Authors.Remove(objFromDb);
+        //    _db.SaveChanges();
+        //    return RedirectToAction(nameof(Index));
+        //}
+    }
+}
