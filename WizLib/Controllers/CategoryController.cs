fdb7f2e0fc682b8f972318ec7caa41cd7e5289dd
Ben Spark <bhrugen1990@gmail.com>
2020-05-11 15:24:36 -0500

    Section 5 - Reading using EF Core

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+    }
+}
