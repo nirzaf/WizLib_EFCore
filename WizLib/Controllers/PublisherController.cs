c6897a56061ad570287d93675d57f20885b2e47c
Ben Spark <bhrugen1990@gmail.com>
2020-05-11 16:43:06 -0500

    Section 5 - Assignmnet Setup Author and Publisher

---
+++ WizLib/Controllers/PublisherController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WizLib_DataAccess.Data;
+using WizLib_DataAccess.Migrations;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PublisherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+         
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            
+            return View();
+            
+        }
+
+        public IActionResult Delete(int id)
+        {
+            
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
