b469d9dcc6620cb5994491264a76268fd52ea8fb
Ben Spark <bhrugen1990@gmail.com>
2020-05-12 16:13:40 -0500

    Secion 8 - Create Sproc and View

---
+++ WizLib_DataAccess/Migrations/20200512210702_AddSprocAndView.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class AddSprocAndView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER VIEW dbo.GetOnlyBookDetails
+                AS
+                SELECT  m.ISBN,m.Title,m.Price FROM dbo.Books m
+            ");
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE dbo.getAllBookDetails   
+                    @bookId int
+                AS   
+
+                    SET NOCOUNT ON;  
+                    SELECT  *  FROM dbo.Books b
+	                WHERE b.Book_Id=@bookId
+                GO  
+            ");
+
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW dbo.GetOnlyBookDetails");
+
+            migrationBuilder.Sql("DROP PROCEDURE dbo.getAllBookDetails");
+        }
+    }
+}
---
+++ WizLib_Model/Models/BookDetailsFromView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizLib_Model.Models
+{
+    public class BookDetailsFromView
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+    }
+}
