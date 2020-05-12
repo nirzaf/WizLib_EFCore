4457fba98b4933bc06be39a02eabc6e7f8ed309e
Ben Spark <bhrugen1990@gmail.com>
2020-05-11 20:50:43 -0500

    Section 6 - Demo - Deferred Execution

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -124,5 +124,31 @@ namespace WizLib.Controllers
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult PlayGround()
+        {
+            var bookTemp = _db.Books.FirstOrDefault();
+            bookTemp.Price = 100;
+
+            var bookCollection = _db.Books;
+            double totalPrice = 0;
+
+            foreach (var book in bookCollection)
+            {
+                totalPrice += book.Price;
+            }
+
+            var bookList = _db.Books.ToList();
+            foreach (var book in bookList)
+            {
+                totalPrice += book.Price;
+            }
+
+            var bookCollection2 = _db.Books;
+            var bookCount1 = bookCollection2.Count();
+
+            var bookCount2 = _db.Books.Count();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
